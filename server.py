58075abbe8732b186818ee676be0471d4188161d
dhivakar je <greenstechnologyllc@gmail.com>
2024-11-26 20:21:40 -0500

    initial set of files

---
+++ server.py
@@ -0,0 +1,13 @@
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Input from user
+num1 = 2
+num2 = 3
+
+# Calculate sum
+result = add_numbers(num1, num2)
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {result}")
